826f6484d5bdd0d1d91ef4cd3c8794278da14366
AJAYREDDY <AJAYREDDY@DESKTOP-JHH56Q4>
2020-06-08 15:40:51 +0530

    temp

---
+++ DemoRest-1/src/main/java/com/javatpoint/model/UserRecord.java
@@ -0,0 +1,44 @@
+package com.javatpoint.model;  
+import javax.persistence.Entity; 
+
+import javax.persistence.Table;  
+ 
+import javax.persistence.Id;  
+@Entity    
+@Table
+public class UserRecord   
+{   
+@Id    
+private int id;    
+private String name;    
+private String email;  
+//default conatructor    
+public UserRecord()  
+{  
+}    
+public int getId()   
+{    
+return id;    
+}    
+public void setId(int id)   
+{    
+this.id = id;    
+}    
+public String getName()   
+{    
+return name;    
+}    
+public void setName(String name)   
+{    
+this.name = name;    
+}    
+public String getEmail()   
+{    
+return email;    
+}    
+public void setEmail(String email)   
+{  
+this.email = email;    
+}    
+}    
+
